b0ec5b54d68cf84712deccbb94ebf57534915ba4
sudhir <sudhir9297@gmail.com>
2023-01-29 10:15:59 +0530

    minor Update

--- src/pages/AlternateColumnScroll.jsx
+++ src/pages/AlternateColumnScroll.jsx
@@ -68,10 +68,7 @@ function Column({ isCenter, num }) {
       <div className="relative block will-change-transform">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i, idx) => (
           <div className="m-0 relative z-10" key={idx}>
-            <div
-              className="relative overflow-hidden rounded-xl cursor-pointer mt-[8vh] mx-[1vw] "
-              data-pos="4"
-            >
+            <div className="relative overflow-hidden rounded-xl cursor-pointer mt-[8vh] mx-[1vw] ">
               <img
                 className="w-full h-full"
                 src={`https://source.unsplash.com/random/400x420/?face&${i}?sig&${
